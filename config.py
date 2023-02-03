f5d83a8c68cc69ac901254c7f587d11250550fd1
LieStage <118051769+LieStage@users.noreply.github.com>
2023-02-04 02:20:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      API_ID = int(os.environ.get("API_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      OWNER_ID = int(os.environ.get("OWNER_ID"))
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "5484278199:AAF9jJuLtDkx1M4QsPJim9KPwaoWCBltO3A")
+      API_ID = int(os.environ.get("API_ID", 4682685))
+      API_HASH = os.environ.get("API_HASH",'3eba5d471162181b8a3f7f5c0a23c307')
+      OWNER_ID = int(os.environ.get("OWNER_ID",945284066))
 
