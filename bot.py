311d92b6327c42cef6f55752c3552e6f36a80ad8
LieStage <118051769+LieStage@users.noreply.github.com>
2023-02-05 12:25:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,3 +25,7 @@ if __name__ == "__main__" :
         workers=100
     )
     app.run()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
